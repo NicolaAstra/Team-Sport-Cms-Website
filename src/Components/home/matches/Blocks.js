5c23ae76504d5cda3c220d221896e2c4aace677f
NicolaAstra <mcsnow78@gmail.com>
2019-07-17 23:54:16 +0700

    add match block"

--- src/Components/home/matches/Blocks.js
+++ src/Components/home/matches/Blocks.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { firebaseMatches } from '../../../firebase';
 import { firebaseLooper, reverseArray } from '../../ui/misc';
 
+import  MatchesBlock  from '../../ui/matches_block';
+import Slide from 'react-reveal/Slide';
+
 class Blocks extends Component {
     state = {
         matches:[]
@@ -17,10 +20,18 @@ class Blocks extends Component {
         })
     }
 
-    showMatches = () => (
-        <div>
-            match
-        </div>
+    showMatches = (matches) => (
+        matches?
+            matches.map((match)=> (
+              <Slide bottom key={match.id}>
+                <div className="item">
+                    <div className="wrapper">
+                        <MatchesBlock Block match={match}/>
+                    </div>
+                </div>
+            </Slide>
+        ))
+        :null
     )
 
     render() {
