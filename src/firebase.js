cedcc3a437ba4911b803a7d336ce7e6c14d35f56
NicolaAstra <mcsnow78@gmail.com>
2019-07-17 13:00:04 +0700

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
+
+const config = {
+    apiKey: "AIzaSyCr3sU425uF2tj8F3tEmc_ZhVXjYvdOap0",
+    authDomain: "sport-team-cms.firebaseapp.com",
+    databaseURL: "https://sport-team-cms.firebaseio.com",
+    projectId: "sport-team-cms",
+    storageBucket: "",
+    messagingSenderId: "454564747465",
+    appId: "1:454564747465:web:612a4d69e8e6f48b"
+  };
+
+firebase.initializeApp(config);
+
+
+const firebaseDB =  firebase.database();
+
+firebaseDB.ref('matches').once('value').then((snapshot)=>{
+    console.log(snapshot.val())
+})
