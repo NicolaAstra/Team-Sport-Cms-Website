42bccb9ce333b871016f30d04c3702f5e18c3ef0
NicolaAstra <mcsnow78@gmail.com>
2019-07-21 21:46:08 +0700

    finish the submit form

--- src/Components/home/promotion/Enroll.js
+++ src/Components/home/promotion/Enroll.js
@@ -3,6 +3,9 @@ import Fade from 'react-reveal/Fade';
 import FormField from '../../ui/formFields';
 import { validate } from '../../ui/misc';
 
+import { firebasePromotions } from '../../../firebase'; 
+
+
 
 
 class Enroll extends Component {
@@ -52,6 +55,32 @@ class Enroll extends Component {
 
     }
 
+    resetFormSuccess(type){
+        const newFormdata = {...this.state.formdata}
+
+        for(let key in newFormdata){
+            newFormdata[key].value = '';
+            newFormdata[key].valid = false;
+            newFormdata[key].validationMessage  = '';
+        }
+
+        this.setState({
+            formError:false,
+            formdata: newFormdata,
+            formSuccess: type ? 'Congratulation' : 'Already on the database'
+        })
+
+        this.successMessage();
+    }
+
+    successMessage(){
+        setTimeout(()=> {
+            this.setState({
+                formSuccess:''
+            })
+        },2000)
+    }
+
     submitForm(event){
         event.preventDefault();
 
@@ -64,7 +93,17 @@ class Enroll extends Component {
         }
 
         if(formIsValid){
-            console.log(dataToSubmit)
+            firebasePromotions.orderByChild('email').equalTo(dataToSubmit.email).once('value')
+            .then((snapshot)=>{
+               if(snapshot.val() === null){
+                firebasePromotions.push(dataToSubmit);
+                this.resetFormSuccess(true);
+               }else{
+                this.resetFormSuccess(false);
+               }
+            })
+
+            // this.resetFormSuccess()
         } else {
             this.setState({
                 formError : true
@@ -94,7 +133,11 @@ class Enroll extends Component {
                                     </div>
                                     :null
                                 }
+                                <div className="success_label">{this.state.formSuccess}</div>
                             <button onClick={(event)=> this.submitForm(event)}>Enroll</button>
+                            <div className="enroll_discl">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint inventore doloribus vero quisquam ipsam!
+                            </div>
                         </div>
                     </form>
                 </div>
