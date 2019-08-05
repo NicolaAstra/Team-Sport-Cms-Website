3eb6f8b79a48d494abeec8f482fe18cbad440436
NicolaAstra <mcsnow78@gmail.com>
2019-08-06 04:44:03 +0700

    add transition in matches component

--- src/Components/theMatches/matchesList.js
+++ src/Components/theMatches/matchesList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { easePolyOut } from 'd3-ease';
+import NodeGroup from 'react-move/NodeGroup';
 
 class MatchesList extends Component {
     state = {
@@ -7,15 +9,67 @@ class MatchesList extends Component {
 
     static getDerivedStateFromProps(props, state){
 
-        return state ={
+        return state = {
             matchesList: props.matches
         }
     }
 
+    showMatches = () => (
+        this.state.matchesList ?
+            <NodeGroup
+                data={this.state.matchesList}
+                keyAccessor={(d)=> d.id}
+
+                start={()=>({
+                    opacity:0,
+                    x:-200
+                })}
+
+                enter={(d,i)=>({
+                    opacity:[1],
+                    x:[0],
+                    timing:{duration: 500, delay: i * 50, ease: easePolyOut}
+                })}
+
+                update={(d,i)=>({
+                    opacity:[1],
+                    x:[0],
+                    timing:{duration: 500, delay: i * 50, ease: easePolyOut}
+                })}
+
+                leave={(d,i)=>({
+                    opacity:[0],
+                    x:[-200],
+                    timing:{duration: 500, delay: i * 50, ease: easePolyOut}
+                })}
+            >
+                {(nodes)=>(
+                    <div>
+                        {  nodes.map(({key, data, state:{x, opacity}})=>(
+                            <div
+                                key={key}
+                                className="match_box_big"
+                                style={{
+                                    opacity,
+                                    transform: `translate(${x}px)`
+                                }}
+                            >
+                                The Match
+                            </div>
+                    
+                        ))}
+                    </div>
+                )}
+
+            </NodeGroup>
+        :null
+    )
+
     render() {
+        console.log(this.state.matchesList)
         return (
             <div>
-                list
+                {this.showMatches()}
             </div>
         );
     }
