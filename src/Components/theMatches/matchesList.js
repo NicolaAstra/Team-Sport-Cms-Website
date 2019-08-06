1291bf9f6900e8028b9e0ffa03bf87a2908b5c1b
NicolaAstra <mcsnow78@gmail.com>
2019-08-06 08:46:50 +0700

    add filter in match componnent

--- src/Components/theMatches/matchesList.js
+++ src/Components/theMatches/matchesList.js
@@ -54,7 +54,28 @@ class MatchesList extends Component {
                                     transform: `translate(${x}px)`
                                 }}
                             >
-                                The Match
+                                        <div className="block_wraper">
+                                            <div className="block">
+                                                <div 
+                                                    className="icon"
+                                                    style={{background:`url(/images/team_icons/${data.localThmb}.png)`}}></div>
+                                                 <div className="team">{data.local}</div>
+                                                 <div className="team">{data.resultLocal}</div>
+                                            </div>
+                                            <div className="block">
+                                                <div 
+                                                    className="icon"
+                                                    style={{background:`url(/images/team_icons/${data.awayThmb}.png)`}}></div>
+                                                 <div className="team">{data.away}</div>
+                                                 <div className="team">{data.resultAway}</div>
+                                            </div>
+                                        </div>
+                                    <div className="block_wraper nfo">
+                                        <div><strong>Date:</strong> {data.date}</div>
+                                        <div><strong>Stadium:</strong> {data.stadium}</div>
+                                        <div><strong>Referee:</strong> {data.referee}</div>
+                                    </div>
+                                    
                             </div>
                     
                         ))}
